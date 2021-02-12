ab970b2a2faa5d1dd6a3e1106d6879b87f1917d6
Swayam Sahu <swayamsahu77@gmail.com>
2021-02-12 17:34:32 +0530

    initial

---
+++ app.js
@@ -0,0 +1,10 @@
+const toggleButton = document.querySelector('.menu-toggler');
+const mobileMenu = document.querySelector('.nav-menu');
+
+const toggleMenu = () => mobileMenu.classList.toggle('hide');
+
+toggleButton.addEventListener('click', toggleMenu)
+
+window.setInterval(function(){
+    
+}, 1000)

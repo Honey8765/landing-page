828c7fa2fd85fc77e9a4efeb09295c9d795966b0
Swayam Sahu <swayamsahu77@gmail.com>
2021-02-13 23:17:56 +0530

    step2

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 const toggleButton = document.querySelector('.menu-toggler');
 const mobileMenu = document.querySelector('.nav-menu');
-
 const toggleMenu = () => mobileMenu.classList.toggle('hide');
-
 toggleButton.addEventListener('click', toggleMenu)
 
+
+var word1 = ['Creativity', 'Passion', 'Art', 'Fans', 'Followers', 'Community']; 
+var word2 = ['$$$', '$$$', '$$$', 'Patrons', 'Patrons', '$$$']; 
+
+var t = setInterval(function() { 
+
+  var randomNumber = Math.round( Math.random() * (word1.length-1) ); 
+
+  $('#change_word_1').html( word1[ randomNumber ] ); 
+  $('#change_word_2').html( word2[ randomNumber ] ); 
+}, 500);
+
+
+
+
+
+
+let res=50, int1=1, int2=1, int3=50;
+const result = document.querySelector('#result');
 const slideValue_1 = document.querySelector("#slidevalue_1");
 const inputSlider_1 = document.querySelector("#inputvalue_1");
 inputSlider_1.oninput = (()=>{
     let value_1 = inputSlider_1.value;
-    slideValue_1.textContent = value_1;
-    slideValue_1.style.left = (value_1/2) + "%";
+    slideValue_1.innerHTML = value_1 + ' k';
+    res=res * value_1 / int1;
+    result.innerHTML = '&#x20b9; ' + res;
     slideValue_1.classList.add("show");
+    int1=value_1;
 });
 inputSlider_1.onblur = (()=>{
     slideValue_1.classList.remove("show");
@@ -22,9 +41,11 @@ const slideValue_2 = document.querySelector("#slidevalue_2");
 const inputSlider_2 = document.querySelector("#inputvalue_2");
 inputSlider_2.oninput = (()=>{
     let value_2 = inputSlider_2.value;
-    slideValue_2.textContent = value_2;
-    slideValue_2.style.left = (value_2/2) + "%";
+    slideValue_2.innerHTML = value_2 + ' %';
+    res=res * value_2 / int2;
+    result.innerHTML = '&#x20b9; ' + res;
     slideValue_2.classList.add("show");
+    int2=value_2;
 });
 inputSlider_2.onblur = (()=>{
     slideValue_2.classList.remove("show");
@@ -35,10 +56,17 @@ const slideValue_3 = document.querySelector("#slidevalue_3");
 const inputSlider_3 = document.querySelector("#inputvalue_3");
 inputSlider_3.oninput = (()=>{
     let value_3 = inputSlider_3.value;
-    slideValue_3.textContent = value_3;
-    slideValue_3.style.left = (value_3/2) + "%";
+    slideValue_3.innerHTML = '&#x20b9; '  + value_3;
+    res=res * value_3 / int3;
+    result.innerHTML = '&#x20b9; ' + res;
     slideValue_3.classList.add("show");
+    int3=value_3;
 });
 inputSlider_3.onblur = (()=>{
     slideValue_3.classList.remove("show");
 });
+
+
+
+
+
